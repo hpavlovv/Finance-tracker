d170a96a3f7af01e678d3d048ec44f36bc4db512
hpavlovv <hpavlov971@gmail.com>
2024-09-03 16:22:55 +0300

    second commit

---
+++ finance-tracker/src/App.js
@@ -0,0 +1,22 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1 style={{textAlign: 'center'}}>This is a basic Finance-tracker.</h1>
+        <p>
+          Income / Month:
+        </p>
+        <p>
+          Expenses:
+        </p>
+        <p>
+          Monthly Savings: 
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
