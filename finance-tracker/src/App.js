e62ff8fdba73afe161316a9b160a1597e2c62c38
hpavlovv <hpavlov971@gmail.com>
2024-09-11 11:10:20 +0300

    Added forms for Food, Shopping and Credit

--- finance-tracker/src/App.js
+++ finance-tracker/src/App.js
@@ -13,25 +13,31 @@ function App() {
   const [income, setIncome] = useState(0);
   const [bills, setBills] = useState(0);
   const [expenses, setExpenses] = useState(0);
+  const [food, setFood] = useState(0);
+  const [shopping, setShopping] = useState(0);
+  const [credit, setCredit] = useState(0);
   const [savings, setSavings] = useState(0);
 
   // Function to calculate remaining income
   const calculateSavings = () => {
-    const remainingIncome = income - (bills + expenses);
+    const remainingIncome = income - (bills + expenses + food + shopping + credit);
     setSavings(remainingIncome);
   };
 
   // Define Pie Chart Data
   const PieChartData = {
-    labels: ["Income / Month", "Bills", "Expenses", "Savings"],
+    labels: ["Income / Month", "Bills", "Expenses", "Food", "Shopping", "Credit", "Savings"],
     datasets: [
       {
         label: "Leva",
-        data: [income, bills, expenses, savings],
+        data: [income, bills, expenses, food, shopping, credit, savings],
         backgroundColor: [
           "rgba(54, 162, 235, 0.2)",
-          "rgba(255, 165, 0, 0.2)",
           "rgba(255, 0, 0, 0.2)",
+          "rgba(255, 255, 255, 0.2)",
+          "rgba(255, 165, 0, 0.2)",
+          "rgba(0, 255, 255,0.2)",
+          "rgba(153, 51, 255,0.2)",
           "rgba(75, 206, 86, 0.2)",
         ],
         hoverOffset: 4,
@@ -59,6 +65,9 @@ function App() {
       <InputField label="Income / Month" onSubmit={setIncome} />
       <InputField label="Bills" onSubmit={setBills} />
       <InputField label="Expenses" onSubmit={setExpenses} />
+      <InputField label="Food" onSubmit={setFood} />
+      <InputField label="Shopping" onSubmit={setShopping} />
+      <InputField label="Credit" onSubmit={setCredit} />
       
       </div>
       <div id="chart-container">
@@ -67,7 +76,7 @@ function App() {
       </div>
 
       <button onClick={calculateSavings}>Calculate Savings</button>
-      <button onClick={() => { setIncome(0); setBills(0); setExpenses(0); setSavings(0); }}>Reset</button>
+      <button onClick={() => { setIncome(0); setBills(0); setExpenses(0); setFood(0); setShopping(0); setCredit(0); setSavings(0); }}>Reset</button>
 
       <h2>Monthly Savings: {savings}</h2>
 
